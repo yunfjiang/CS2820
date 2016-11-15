844cceebe0db34b0ae089232265a34dd5f723224
hekemp <heather-kemp@uiowa.edu>
2016-11-15 11:27:22 -0600

    Create MockBelt.java

---
+++ MockObjects/MockBelt.java
@@ -0,0 +1,45 @@
+
+/**
+ * 
+ * @author Heather Kemp
+ * the MockBelt.java class is a mock version of the Belt.java object, created simply to test the Master's ability to
+ * cycle through events.
+ * It only implements the methods required by the Event interface.
+ *
+ */
+
+
+public class MockBelt implements Event{
+	
+	/**
+	 * Constructor initializes nothing, as everything is handled in the mock methods.
+	 */
+	
+	public MockBelt(){
+	}
+	
+	/**
+	 * Performs an action. Extracts the method to be executed and parameters from the method strong and then executes these
+	 * methods.
+	 * @input - a string which contains a method name/key and parameters, seperated by commas
+	 */
+	public void performAction(String Method){
+	    System.out.println("Belt did something.");
+	}
+	
+	/**
+	 * Gets the event. Returns this object as an Event
+	 */
+	
+	public Event getEvent(){
+
+	     return (Event) this;
+	   }
+	/**
+	 * Gets the parameters. Returns the parameter for the next call of this method.
+	 */
+	
+	public String getPara(){
+	    return "Null";
+	   }
+}

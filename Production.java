05a171cd2ee695783de609d6442302df03ff9d56
hekemp <heather-kemp@uiowa.edu>
2016-11-18 09:19:39 -0600

    Update Production.java

--- Production.java
+++ Production.java
@@ -1,3 +1,13 @@
+package production;
+
+/**
+ * 
+ * @author Heather Kemp
+ * The Production class is the true 'main' class which contains the Master object and tells it to run with the objects that it has created.
+ * It contains only one method, it's static void main(String[] args) method, which will create the needed objects and then run the simulation.
+ 
+ *
+ */
 
 public class Production
 {
@@ -11,6 +21,13 @@ public class Production
         Visualizer newVisualizer = new Visualizer();
         
         Master myMaster = new Master();
+        myMaster.addBelt(newBelt);
+        myMaster.addFloor(newFloor);
+        myMaster.addInventory(newInventory);
+        myMaster.addOrder(newOrder);
+        myMaster.addRobot(newRobot);
+        myMaster.addShelf(newShelf);
+        myMaster.addVisualizer(newVisualizer);
         myMaster.runSimulation();
     }
 }

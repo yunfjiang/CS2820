37f3234516c2184d733f96ce9b82642af1f8adb4
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-18 10:24:54 -0500

    Update and rename Regularship to Regularship.java

--- Regularship.java
+++ Regularship.java
@@ -1,3 +1,11 @@
+package Production;
+
+/**
+ * 
+ * @author hyo kyung kang
+ *
+ */
+ 
 public class Regularship extends Orders {
 
 	public Regularship(String progress, String address, String dateReceived, int orderNumber) {

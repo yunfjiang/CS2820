2ea17fbbaedcd01e9309e89c82e01415825a8cb3
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-18 09:47:59 -0500

    Create OrdersTest.java

---
+++ OrdersTest.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ *  * @author hyo kyung kang
+ *
+ */
+public class OrdersTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	/*
+	 * just test the methods I made
+	 * Imaginary input compare Heapsorter   
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

f3aa167765b795d9e33c2c526288c426aeaf501c
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-18 09:44:00 -0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+	String cusname;
+	int ID;
+	int password;
+	
+ public Customer(String Username,int ID,int password){
+	 cusname=Username;
+	 this.ID=ID;
+	 this.password=password;
+ }
+}
